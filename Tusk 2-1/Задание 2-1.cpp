e64e4f9ca49bce4cd3ceec0220f304b2d1984494
KIrillTeleshin <ktelesin@gmail.com>
2021-12-16 14:09:22 +0300

    Переделал программу

--- Tusk 2-1/Задание 2-1.cpp
+++ Tusk 2-1/Задание 2-1.cpp
@@ -1,39 +1,38 @@
-#include <iostream>
 #include <cmath>
-
-double cathet1, cathet2;	//Designation of global variablesCalculation of the triangle perimeter
-//ACalculation of the triangle perimeter
-//Return 0 If no errors
-  int
-CalculatingPerimeter ()
-{
-  double Perimeter;
-  Perimeter =cathet1 + cathet2 + sqrt ((pow (cathet1, 2)) + (pow (cathet2, 2)));
-  std::cout << "perimeter=" << Perimeter << std::endl;
-  return 0;
-}
-
-
-//Calculating the area of a triangle
-//Return 0 If no errors
-int
-CalculatingArea ()
-{
-  double Area;
-  Area = cathet1 * cathet2 / 2;
-  std::cout << "Area=" << Area << std::endl;
-  return 0;
-}
-
-//Brief Entry point
-//Return 0 If no errors
-int main ()
-{
-  std::cout << "enter a cathet1 ";	//enter the value of two variables cathet1 and  cathet2
-  std::cin >> cathet1;
-  std::cout << "enter a cathet2: ";
-  std::cin >> cathet2;
-  CalculatingArea ();
-  CalculatingPerimeter ();
-  return 0;
-}
+#include <iostream>
+/**
+* \brief calculation of a given function CalculatingPerimeter(const double cathet1, const double cathet2)
+* \ param function variables edge
+* \ return calculated value of the function CalculatingPerimeter
+*/
+	  double CalculatingPerimeter(const double cathet1, const double cathet2);
+  /**
+  * \brief calculation of a given function CalculatingPerimeter(const double cathet1, const double cathet2)
+  * \ param function variables edge
+  * \ return calculated value of the function CalculatingPerimeter
+  */
+  double CalculatingArea(const double cathet1, const double cathet2);
+  /**
+  * \ brief function main
+  * \ param
+  * \return 0 if the program is executed
+  */
+  int  main()
+  {
+	  double cathet1, cathet2;
+	  std::cout << "enter a cathet1 ";
+	  std::cin >> cathet1;
+	  std::cout << "enter a cathet2: ";
+	  std::cin >> cathet2;
+	  std::cout << "Perimeter=" << CalculatingPerimeter(cathet1, cathet2) << std::endl;
+	  std::cout << "Area=" << CalculatingArea(cathet1, cathet2);
+	  return 0;
+  }
+  double CalculatingPerimeter(const double cathet1, const double cathet2)
+  {
+	  return cathet1 + cathet2 + sqrt((pow(cathet1, 2)) + (pow(cathet2, 2)));
+  }
+  double CalculatingArea(const double cathet1, const double cathet2)
+  {
+	  return cathet1 * cathet2 / 2;
+  }
