3e9daba349c99c651327475b80164bd7fcf21887
KIrillTeleshin <ktelesin@gmail.com>
2021-12-16 14:14:53 +0300

    Переделал программу

--- Tusk 1-3/Задание 1-3.cpp
+++ Tusk 1-3/Задание 1-3.cpp
@@ -1,29 +1,27 @@
-#include <iostream>
 #include <cmath>
-
-const double g =9.8;
-double weight;		//Designation of global variables
-
-
-//Notation of the formula for finding gravity
-//Return 0 If no errors
-int
-CalculatingGravity ()
+#include <iostream>
+/**
+*\brief bag weight
+*\param  - the value of mass and the value of the gravitational constant
+*\return gravity
+*/
+double weight(const double weight1, const double g);
+/**
+* \ brief function main
+* \ param
+* \return 0 if the program is executed
+*/
+int main()
 {
-  double Gravity;
-  Gravity=g*weight;
-  std::cout << "bag weight="<< Gravity << std::endl;
-  std::cout << "Gravity="<<Gravity<< std::endl;
-  return 0;
+	double weight1;
+	const double g = 9.8;
+	std::cout << "enter the mass-m=";
+	std::cin >> weight1;
+	std::cout << "bag weight=" << weight(weight1, g) << std::endl;
+	std::cout << "Gravity=" << weight(weight1, g);
+	return 0;
 }
-
-//Brief Entry point
-//Return 0 If no errors
-int
-main ()
+double weight(const double weight1, const double g)
 {
-  std::cout << "enter the mass-m";	//
-  std::cin >> weight;
-  CalculatingGravity ();
-  return 0;
+	return (g * weight1);
 }
