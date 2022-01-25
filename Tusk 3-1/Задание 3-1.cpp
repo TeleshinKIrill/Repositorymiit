31219ccfac13c70d9e1409cbbf1bc89ebd68bc49
KIrillTeleshin <ktelesin@gmail.com>
2022-01-25 04:27:06 +0300

    .

--- Tusk 3-1/Задание 3-1.cpp
+++ Tusk 3-1/Задание 3-1.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 #include <cmath>
-/**
-*\ brief  The volume of the Parallelepiped
-*\ param width,length,height
-*\ return volume
+#include <iomanip>
+
+
+
+/*
+*\brief Finds the value of the function.
+*\param x Meaning x.
+*\param a Meaning a=0.29.
+*\param b Meaning b=1.2502.
+*\return Meaning y.
 */
-double funct(double x);
+double GetY(double x, double const a, double const b);
+
 /**
-*\ brief  The area of the Parallelepiped
-*\ param width,length,height
-*\ return  area
+* \brief Entry point to the program
+* \return Error code (0) success
 */
 int main()
 {
-    double x;
-    for (int i = 0; i <= 10; i++)
+    double const a = 0.29, b = 1.2502;
+    double left, right, step;
+    std::cout << "Enter left limit of interval- ";
+    std::cin >> left;
+    std::cout << "Enter right limit of interval- ";
+    std::cin >> right;
+    std::cout << "Enter value of step- ";
+    std::cin >> step;
+
+    double x = left;
+    while (x <= right)
     {
-        x = 0.0 + 0.2 * i;
-        std::cout << "For x=" << x << " y= " << funct(x) << "\n";
+        std::cout << std::setprecision(5) << GetY(x, a, b) << std::endl;
+        x += step;
     }
-    return 1;
+    return 0;
 }
-double funct(double x)
+
+double GetY(double x, double const a, double const b)
 {
-    return 0.29 * pow(x, 3) + x - 1.2502;
+    return a * pow(x, 3) + x - b;
 }
