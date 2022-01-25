9e5cc45deefd8a3f8ea48daeb799ce96f072efa8
KIrillTeleshin <ktelesin@gmail.com>
2022-01-25 04:03:44 +0300

    .

--- Tusk 2-1/Задание 2-1.cpp
+++ Tusk 2-1/Задание 2-1.cpp
@@ -1,66 +1,78 @@
-#define _USE_MATH_DEFINES // for C++
+#define _USE_MATH_DEFINES 
 #include <iostream>
 #include <iomanip>
 #include <cmath>
 
-/**
- * \brief Calculation of the triangle perimeter.
- * \param cathet1 cathet2 triangle cathets.
- * \return Triangle perimeter.
- */
-double CalculatingPerimeter(const double cathet1, const double cathet2);
-
 
 /**
-* \brief Calculation of the triangle Area.
- * \param cathet1 cathet2 triangle cathets.
- * \return Triangle Area.
+* \brief Calculation of the perimeter of a right triangle
+ * \param First Cathet - Length of the first cathet
+ * \param Second Cathet - Length of the second cathet
+ * \return The perimeter of a right triangle
  */
-double CalculatingArea(const double cathet1, const double cathet2);
+double getPerimeter(double FirstCathet, double SecondCathet);
 
 /**
-* \brief User choice of calculating either the Area of the triangle (0),
-* or the surface Perimeter of the triangle (1)
+ * \brief Calculation of the area of a right triangle
+ * \param First Cathet - Length of the first cathet
+ * \param Second Cathet - Length of the second cathet
+ * \return The area of a right triangle
  */
-enum class ActionChoice { Area, Perimeter };
+double getArea(double FirstCathet, double SecondCathet);
 
 /**
- * \brief The entry point to the program.
- * \return Error code (0 - success).
+ *\brief User choice of perimeter calculation (1) or area (0)
  */
+enum class State { perimeter, area };
+
+/**
+* \brief Program entry point
+* \return Error code (0) success
+*/
 int main()
 {
-    double cathet1, cathet2;
-    std::cout << "enter a cathet1= ";
-    std::cin >> cathet1;
-    std::cout << "enter a cathet2= ";
-    std::cin >> cathet2;
 
-    std::cout << "Enter an action on the triangle (Area = 0, Perimeter = 1) ";
-    int input;
-    std::cin >> input;
+	double FirstCathet, SecondCathet;
+	std::cout << "Length of the first cathet - ";
+	std::cin >> FirstCathet;
+	std::cout << "Length of the second cathet - ";
+	std::cin >> SecondCathet;
+	std::cout << "Select an action (to calculate the area, select 0, to calculate the perimeter, select 1)";
+	int input;
+	std::cin >> input;
+	const auto choice = static_cast<State>(input);
 
-    const auto choice = static_cast<ActionChoice>(input);
 
-    switch (choice)
-    {
-    case ActionChoice::Perimeter:
-    {
-        const auto Perimeter = CalculatingPerimeter(cathet1, cathet2);
-        std::cout << "the perimeter of the triangle = " << Perimeter;
-        break;
-    }
-    case ActionChoice::Area:
-    {
-        const auto Area = CalculatingArea(cathet1, cathet2);
-        std::cout << "the Area of the triangle  = " << Area;
-        break;
-    }
-    }
-    std::cout << std::endl;
-    return 0;
+	switch (choice)
+	{
+	case State::perimeter:
+	{
+		const double Perimeter = getPerimeter(FirstCathet, SecondCathet);
+
+		std::cout << "The perimeter of the triangle = " << Perimeter;
+		break;
+	}
+	case State::area:
+	{
+		const double Area = getArea(FirstCathet, SecondCathet);
+		std::cout << "The area of the triangle = " << Area;
+		break;
+	}
+	}
+	return 0;
 }
-double CalculatingPerimeter(const double cathet1, const double cathet2)
+
+
+double getPerimeter(double FirstKatet, double SecondKatet)
+{
+	return FirstKatet + SecondKatet + sqrt(FirstKatet * FirstKatet + SecondKatet * SecondKatet);
+}
+
+
+double getArea(double FirstKatet, double SecondKatet)
+{
+	return FirstKatet * SecondKatet / 2;
+}
 {
     return cathet1 + cathet2 + sqrt((pow(cathet1, 2)) + (pow(cathet2, 2)));
 }
